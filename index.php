fe6f1492ed6dac9d7424be43030f9a0cd39ce01a
LunguGeorgeProgramator <51510106+LunguGeorgeProgramator@users.noreply.github.com>
2020-09-16 13:01:30 +0300

    changes to my request class

--- index.php
+++ index.php
@@ -6,7 +6,49 @@ $request = new httpRequest;
 
 // $request->requestHeaders['accept-encoding'] = 'gzip, deflate, br';
 
-$response = $request->request("https://www.google.com/", "GET"); // GET example
+
+$apiKey = '724dcd02791a41529148954bca4524fb'; /// use your apiKey and replace XXXX string
+
+/// example of a POST api request
+
+// $request->requestHeaders = [
+//     'Accept' => 'text/html',
+//     'Content-Type' => 'application/x-www-form-urlencoded'
+// ];
+
+// $request->queriesArray = [
+//     'apiKey' => $apiKey
+// ];
+
+// $response = $request->request(
+//     "https://api.spoonacular.com/recipes/visualizeIngredients?apiKey=" . $apiKey, 
+//     "POST", 
+//     [
+//         'spoonacularApiKey' => $apiKey, 
+//         'servings' => 2,
+//         'measure' => 'metric',
+//         'viewStyle' => 'grid'
+//     ]); // example request food api
+
+
+/// example of a GET api request
+
+// $request->requestHeaders = [
+//     'Content-Type' => 'application/json'
+// ];
+
+// $request->queriesArray = [
+//     'apiKey' => $apiKey
+// ];
+
+// $response = $request->request(
+//     "https://api.spoonacular.com/recipes/4632/summary", 
+//     "GET"
+// ); // GET example
+
+/// example of a simple GET request
+
+$response = $request->request("http://www.google.com");
 
 echo $response;
 
